0cfe5e489edf308f62d4a1cb14ab03baf707d73e
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-11-07 21:13:22 +0000

    Formatted code

--- pages/api/modern-warfare-two/secondary.tsx
+++ pages/api/modern-warfare-two/secondary.tsx
@@ -1,9 +1,9 @@
-import secondaryList from '../../../json/modern-warfare-two/secondary.json';
-import { randomListItem } from '../../../helpers/randomListItem';
-import { Weapon } from '@/app/lib/definitions';
+import secondaryList from "../../../json/modern-warfare-two/secondary.json";
+import { randomListItem } from "../../../helpers/randomListItem";
+import { Weapon } from "@/app/lib/definitions";
 
 export default async function handler(req, res) {
-    const secondary: Weapon = randomListItem(secondaryList);
+  const secondary: Weapon = randomListItem(secondaryList);
 
-    res.status(200).json(secondary);
+  res.status(200).json(secondary);
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,42 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import Header from '@/components/Header';
-import GeneratorCard from '@/components/GeneratorCard';
+import { Container, Row, Col } from "react-bootstrap";
+import Header from "@/components/Header";
+import GeneratorCard from "@/components/GeneratorCard";
 
 export default function Home() {
-    return <>
-        <Header />
-        <Container fluid>
-            <Row>
-                <Col xl={3} lg={4} md={6}>
-                    <GeneratorCard
-                        title='Black Ops 6'
-                        text='Coming in November 2024'
-                        variant='black-ops'
-                        link='black-ops-six/generator'
-                        disabled={true} />
-                </Col>
-                <Col xl={3} lg={4} md={6}>
-                    <GeneratorCard
-                        title='Warzone 2'
-                        text='Coming this summer'
-                        variant='success'
-                        link='warzone-two/generator'
-                        disabled={false} />
-                </Col>
-                <Col xl={3} lg={4} md={6}>
-                    <GeneratorCard
-                        title='Modern Warfare 3 (2023)'
-                        text='Coming this summer'
-                        variant='danger'
-                        link='modern-warfare-three/generator'
-                        disabled={true} />
-                </Col>
-            </Row>
-        </Container>
-    </>;
+  return (
+    <>
+      <Header />
+      <Container fluid>
+        <Row>
+          <Col xl={3} lg={4} md={6}>
+            <GeneratorCard
+              title="Black Ops 6"
+              text="Coming in November 2024"
+              variant="black-ops"
+              link="black-ops-six/generator"
+              disabled={true}
+            />
+          </Col>
+          <Col xl={3} lg={4} md={6}>
+            <GeneratorCard
+              title="Warzone 2"
+              text="Coming this summer"
+              variant="success"
+              link="warzone-two/generator"
+              disabled={false}
+            />
+          </Col>
+          <Col xl={3} lg={4} md={6}>
+            <GeneratorCard
+              title="Modern Warfare 3 (2023)"
+              text="Coming this summer"
+              variant="danger"
+              link="modern-warfare-three/generator"
+              disabled={true}
+            />
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }

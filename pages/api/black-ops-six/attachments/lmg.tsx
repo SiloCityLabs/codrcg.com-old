8c8f7733d7d9cbc299e0e58c62f8c40e906109e7
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-11-18 20:47:02 +0000

    B06: Added all LMGs to LMG attachments API

---
+++ pages/api/black-ops-six/attachments/lmg.tsx
@@ -0,0 +1,57 @@
+import gpmg7 from "@/json/black-ops-six/attachments/lmg/gpmg7.json";
+import pu21 from "@/json/black-ops-six/attachments/lmg/pu21.json";
+import xmg from "@/json/black-ops-six/attachments/lmg/xmg.json";
+import { randomListItem } from "@/helpers/randomListItem";
+
+export default async function handler(req, res) {
+  let attachments: any = {};
+  const body = req.body;
+  const data = getGunAttachments(body.gun);
+
+  if (data) {
+    randomizeAttachments(attachments, data, body.count);
+  }
+
+  res.status(200).json("");
+}
+
+function getGunAttachments(gun: string) {
+  switch (gun) {
+    case "gpmg7":
+      return gpmg7;
+    case "pu21":
+      return pu21;
+    case "xmg":
+      return xmg;
+    default:
+      return null;
+  }
+}
+
+/**
+ * Randomly selects attachments from a pool of data.
+ *
+ * @param {object} attachArr - The object to store the selected attachments.
+ * @param {object} data - The pool of attachments to choose from.
+ * @param {number} count - The number of attachments to select.
+ *
+ * @returns {void}
+ */
+function randomizeAttachments(attachArr: any, data: any, count: number) {
+  let attachs = 0;
+
+  while (attachs < count) {
+    const keys = Object.keys(data);
+    const randomIndex = Math.floor(Math.random() * keys.length);
+    const randomKey = keys[randomIndex];
+
+    if (attachArr.hasOwnProperty(randomKey)) {
+      console.log("randomizeAttachments: already exists");
+      continue;
+    } else {
+      console.log("randomizeAttachments: doesnt already exist");
+      attachArr[randomKey] = randomListItem(data[randomKey]);
+      attachs++;
+    }
+  }
+}

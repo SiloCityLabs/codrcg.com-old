c05bfb2233ab5a41fd801b78a57e381d29ce5672
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-11-19 15:04:55 +0000

    BO6: Added New Season 1 perks

--- types/Generator.d.ts
+++ types/Generator.d.ts
@@ -3,4 +3,12 @@ export type Weapon = {
     type: string;
     game: string;
     no_attach?: boolean;
+    isDlc?: boolean;
+}
+
+export type Perk = {
+    name: string;
+    type: string;
+    game: string;
+    isDlc?: boolean;
 }

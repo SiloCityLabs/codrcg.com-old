19cea543967e99d86c6b4c672d2cb0340482b579
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-11-20 17:51:21 +0000

    ISSUE-18 - BO6: Updated all attachments for Ars besides the Krig C

--- pages/api/black-ops-six/attachments/assault_rifle.tsx
+++ pages/api/black-ops-six/attachments/assault_rifle.tsx
@@ -1,9 +1,9 @@
+//AMES85, GPR91, Model L all use default556.json
+import default556 from "@/json/black-ops-six/attachments/assault_rifle/default556.json";
+//Custom ARs
 import ak74 from "@/json/black-ops-six/attachments/assault_rifle/ak74.json";
-import ames85 from "@/json/black-ops-six/attachments/assault_rifle/ames85.json";
 import asVal from "@/json/black-ops-six/attachments/assault_rifle/asVal.json";
 import goblinMk2 from "@/json/black-ops-six/attachments/assault_rifle/goblinMk2.json";
-import gpr91 from "@/json/black-ops-six/attachments/assault_rifle/gpr91.json";
-import modelL from "@/json/black-ops-six/attachments/assault_rifle/modelL.json";
 import xm4 from "@/json/black-ops-six/attachments/assault_rifle/xm4.json";
 //DLC Weapons
 import krigC from "@/json/black-ops-six/attachments/assault_rifle/krigC.json";
@@ -22,21 +22,17 @@ export default async function handler(req, res) {
 }
 
 function getGunAttachments(gun: string) {
-  return xm4;
-
   switch (gun) {
+    case "ames85":
+    case "gpr91":
+    case "modell":
+      return default556;
     case "ak74":
       return ak74;
-    case "ames85":
-      return ames85;
     case "asval":
       return asVal;
     case "goblinmk2":
       return goblinMk2;
-    case "gpr91":
-      return gpr91;
-    case "modell":
-      return modelL;
     case "xm4":
       return xm4;
     case "krigc":

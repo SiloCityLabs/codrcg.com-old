31526bd980114a32697f31eb7062b48fc8e2fe9c
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-11-22 23:54:03 +0000

    BO6: Cleanup of dlc weapons and all weapon attachments

--- helpers/generator/black-ops-six/attachments/getShotgunAttachments.tsx
+++ helpers/generator/black-ops-six/attachments/getShotgunAttachments.tsx
@@ -1,7 +1,5 @@
 import asg89 from "@/json/black-ops-six/attachments/shotgun/asg89.json";
 import marineSp from "@/json/black-ops-six/attachments/shotgun/marineSp.json";
-//DLC Weapons
-import maelstrom from "@/json/black-ops-six/attachments/shotgun/maelstrom.json";
 import { randomizeAttachments } from "@/helpers/randomizeAttachments";
 
 export function getShotgunAttachments(gun: string, count: number) {
@@ -21,8 +19,6 @@ function getGunAttachments(gun: string) {
       return asg89;
     case "marinesp":
       return marineSp;
-    case "maelstrom":
-      return maelstrom;
     default:
       return null;
   }
--- helpers/generator/black-ops-six/attachments/getSniperAttachments.tsx
+++ helpers/generator/black-ops-six/attachments/getSniperAttachments.tsx
@@ -1,8 +1,6 @@
 import lr762 from "@/json/black-ops-six/attachments/sniper/lr762.json";
 import lw3a1Frostline from "@/json/black-ops-six/attachments/sniper/lw3a1Frostline.json";
 import svd from "@/json/black-ops-six/attachments/sniper/svd.json";
-//DLC Weapons
-import amrMod4 from "@/json/black-ops-six/attachments/sniper/amrMod4.json";
 import { randomizeAttachments } from "@/helpers/randomizeAttachments";
 
 export function getSniperAttachments(gun: string, count: number) {
@@ -24,8 +22,6 @@ function getGunAttachments(gun: string) {
       return lw3a1Frostline;
     case "svd":
       return svd;
-    case "amrmod4":
-      return amrMod4;
     default:
       return null;
   }

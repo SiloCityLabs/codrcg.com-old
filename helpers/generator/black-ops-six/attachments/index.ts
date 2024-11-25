5423b03e07c6c4cc63c1243f27790218ff681377
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-11-25 21:37:03 +0000

    BO6: Cleaned up the getBO6Attachments function

---
+++ helpers/generator/black-ops-six/attachments/index.ts
@@ -0,0 +1,8 @@
+export { getAssaultRifleAttachments } from './getAssaultRifleAttachments';
+export { getSmgAttachments } from './getSmgAttachments';
+export { getLmgAttachments } from './getLmgAttachments';
+export { getMarksmanRifleAttachments } from './getMarksmanRifleAttachments';
+export { getPistolAttachments } from './getPistolAttachments';
+export { getShotgunAttachments } from './getShotgunAttachments';
+export { getSniperAttachments } from './getSniperAttachments';
+export { getSpecialAttachments } from './getSpecialAttachments';

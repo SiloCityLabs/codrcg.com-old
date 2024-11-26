342b84f58db7cce63279f14700cb6d1d9c3e67b8
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-11-26 14:28:13 +0000

    Making functions more effecient

--- helpers/generator/getPerkList.tsx
+++ helpers/generator/getPerkList.tsx
@@ -1,29 +1,31 @@
 import bo6Perk1List from "@/json/black-ops-six/perk/perk1.json";
 import bo6Perk2List from "@/json/black-ops-six/perk/perk2.json";
 import bo6Perk3List from "@/json/black-ops-six/perk/perk3.json";
-import warzonePerk1List from "@/json/black-ops-six/perk/perk1.json";
-import warzonePerk2List from "@/json/black-ops-six/perk/perk2.json";
-import warzonePerk3List from "@/json/black-ops-six/perk/perk3.json";
+// Assuming these imports should be from the warzone directory
+import warzonePerk1List from "@/json/warzone/perk/perk1.json";
+import warzonePerk2List from "@/json/warzone/perk/perk2.json";
+import warzonePerk3List from "@/json/warzone/perk/perk3.json";
 
-export function getPerkList(game: string) {
-  switch (game) {
-    case "black-ops-six":
-      return {
-        perk1List: bo6Perk1List,
-        perk2List: bo6Perk2List,
-        perk3List: bo6Perk3List,
-      };
-    case "warzone":
-      return {
-        perk1List: warzonePerk1List,
-        perk2List: warzonePerk2List,
-        perk3List: warzonePerk3List,
-      };
-    default:
-      return {
-        perk1List: {},
-        perk2List: {},
-        perk3List: {},
-      };
-  }
+const perks: Record<
+  string,
+  { perk1List: any; perk2List: any; perk3List: any }
+> = {
+  "black-ops-six": {
+    perk1List: bo6Perk1List,
+    perk2List: bo6Perk2List,
+    perk3List: bo6Perk3List,
+  },
+  warzone: {
+    perk1List: warzonePerk1List,
+    perk2List: warzonePerk2List,
+    perk3List: warzonePerk3List,
+  },
+};
+
+export function getPerkList(game: string): {
+  perk1List: any;
+  perk2List: any;
+  perk3List: any;
+} {
+  return perks[game] || { perk1List: {}, perk2List: {}, perk3List: {} };
 }

ee20f04fef8d0e2dbd71bd5bd6685711102f1550
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-12-24 17:02:02 +0000

    Implemented Cache-Control

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*", // Match all paths
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, stale-while-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

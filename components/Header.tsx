bead8fbe151665acbeccab0d8cdd45c76a43ce8a
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-12-27 15:03:38 +0000

    Cleaned up header and updated some npm libraries

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../public/styles/main.css";

52e74dfeed5b6f4e0f1a53c60d48f42ed7774fac
aelbaneh0615 <aelbaneh0615@gmail.com>
2024-12-30 18:53:32 +0000

    Implemented GA

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-
+//Hooks
+import useGoogleAnalytics from "../hooks/useGoogleAnalytics";
+//Styles
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../public/styles/main.css";
 import "../public/styles/material.css";
@@ -20,36 +22,38 @@ const defaultNavLinks = [
   },
 ];
 
-export default class Header extends React.Component<HeaderProps> {
-  render() {
-    const { className, navLinks = defaultNavLinks } = this.props;
+function Header(props: HeaderProps) {
+  const { className, navLinks = defaultNavLinks } = props;
+  //Implement Analytics
+  useGoogleAnalytics("G-F3RTW20Q7N");
 
-    return (
-      <Navbar
-        id="cod-header"
-        expand="lg"
-        bg="dark"
-        data-bs-theme="dark"
-        className={`${className}`}
-      >
-        <Container>
-          <Navbar.Brand href="/">COD RCG</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              {navLinks.map((link, index) => (
-                <Nav.Link
-                  key={index}
-                  href={link.href}
-                  target={link.target ? link.target : "_self"}
-                >
-                  {link.label}
-                </Nav.Link>
-              ))}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
-  }
+  return (
+    <Navbar
+      id="cod-header"
+      expand="lg"
+      bg="dark"
+      data-bs-theme="dark"
+      className={`${className}`}
+    >
+      <Container>
+        <Navbar.Brand href="/">COD RCG</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {navLinks.map((link, index) => (
+              <Nav.Link
+                key={index}
+                href={link.href}
+                target={link.target ? link.target : "_self"}
+              >
+                {link.label}
+              </Nav.Link>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default Header;

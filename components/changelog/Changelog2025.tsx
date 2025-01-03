fec58dad289945050bef44cf2a8cf98c4385fc49
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-01-03 18:42:42 +0000

    Updated the changelog to handle the new year

---
+++ components/changelog/Changelog2025.tsx
@@ -0,0 +1,18 @@
+import { Row, Col } from "react-bootstrap";
+
+function Changelog2024() {
+  return (
+    <>
+      <Row>
+        <Col>
+          <p>
+            01/04/2025: <a href="black-ops-three/generator">Black Ops 3:</a>{" "}
+            Generator is now available!
+          </p>
+        </Col>
+      </Row>
+    </>
+  );
+}
+
+export default Changelog2024;
--- pages/changelog.tsx
+++ pages/changelog.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Head from "next/head";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import Header from "@/components/Header";
 import Changelog2024 from "@/components/changelog/Changelog2024";
+import Changelog2025 from "@/components/changelog/Changelog2025";
 
 export default function Changelog() {
+  const [key, setKey] = useState<string>("2024");
+
   return (
     <>
       <Head>
@@ -23,8 +27,21 @@ export default function Changelog() {
         <Row>
           <Col>
             <h2 className="text-center">Changelog</h2>
-
-            <Changelog2024 />
+            <Container className="changelog shadow-lg p-3 mb-5 bg-body rounded">
+              <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k ?? "2025")}
+                className="mb-3"
+              >
+                <Tab eventKey="2024" title="2024">
+                  <Changelog2024 />
+                </Tab>
+                <Tab eventKey="2025" title="2025">
+                  <Changelog2025 />
+                </Tab>
+              </Tabs>
+            </Container>
           </Col>
         </Row>
       </Container>

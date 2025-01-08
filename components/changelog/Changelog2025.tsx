26b12dc84b015ae8c4f5f03252b15cbe6dafa25b
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-01-08 17:33:31 +0000

    Fixed linking on changelog page

--- components/changelog/Changelog2025.tsx
+++ components/changelog/Changelog2025.tsx
@@ -7,19 +7,19 @@ function Changelog2024() {
         <Col>
           <p>
             01/11/2025:{" "}
-            <a href="/black-ops-six/info">Black Ops 6 Generator Information</a>
+            <a href="black-ops-six/info">Black Ops 6 Generator Information</a>
           </p>
           <p>
             01/10/2025:{" "}
-            <a href="/black-ops-four/zombies-generator">Black Ops 4 Zombies:</a>{" "}
+            <a href="black-ops-four/zombies-generator">Black Ops 4 Zombies:</a>{" "}
             Generator is now available!
           </p>
           <p>
-            01/09/2025: <a href="/black-ops-four/generator">Black Ops 4:</a>{" "}
+            01/09/2025: <a href="black-ops-four/generator">Black Ops 4:</a>{" "}
             Generator is now available!
           </p>
           <p>
-            01/04/2025: <a href="/black-ops-three/generator">Black Ops 3:</a>{" "}
+            01/04/2025: <a href="black-ops-three/generator">Black Ops 3:</a>{" "}
             Generator is now available!
           </p>
         </Col>

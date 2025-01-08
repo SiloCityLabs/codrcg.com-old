04d2ccb4f5a9696767154555a053b663344b99a7
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-01-08 15:39:57 +0000

    [ISSUE-85] BO6: Created wildcard list page

---
+++ helpers/info/getWildcards.tsx
@@ -0,0 +1,12 @@
+import { getWildcardList } from "@/helpers/generator/getWildcardList";
+import { Wildcard } from "@/types/Generator";
+
+export function getWildcards(
+  game: string = "all",
+  perk: string = ""
+): Wildcard | Wildcard[] {
+  const data = getWildcardList(game) as Wildcard[];
+  console.log(data);
+
+  return data;
+}

2d8be84b17b063185400a242f1b9bcaa40cb27fb
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-01-18 17:58:40 +0000

    [ISSUE-54] MW3: Updated weapons without no attachment info

--- helpers/generator/modern-warfare-three/attachments/get/getPistolAttachments.tsx
+++ helpers/generator/modern-warfare-three/attachments/get/getPistolAttachments.tsx
@@ -1,13 +1,6 @@
-import renetti from "@/json/modern-warfare-three/attachments/pistol/renetti.json";
-import tyr from "@/json/modern-warfare-three/attachments/pistol/tyr.json";
-import wspstinger from "@/json/modern-warfare-three/attachments/pistol/wspStinger.json";
 import { randomizeAttachments } from "@/helpers/randomizeAttachments";
 
-const attachmentsList: Record<string, any> = {
-  renetti,
-  tyr,
-  wspstinger,
-};
+const attachmentsList: Record<string, any> = {};
 
 export function getPistolAttachments(gun: string, count: number): any {
   const attachments: any = {};
--- helpers/generator/modern-warfare-three/attachments/get/getSniperAttachments.tsx
+++ helpers/generator/modern-warfare-three/attachments/get/getSniperAttachments.tsx
@@ -1,17 +1,6 @@
-import xrkstalker from "@/json/modern-warfare-three/attachments/sniper/xrkStalker.json";
-import mors from "@/json/modern-warfare-three/attachments/sniper/mors.json";
-import kattamr from "@/json/modern-warfare-three/attachments/sniper/kattamr.json";
-import longbow from "@/json/modern-warfare-three/attachments/sniper/longbow.json";
-import kvinhibitor from "@/json/modern-warfare-three/attachments/sniper/kvInhibitor.json";
 import { randomizeAttachments } from "@/helpers/randomizeAttachments";
 
-const attachmentsList: Record<string, any> = {
-  xrkstalker,
-  mors,
-  kattamr,
-  longbow,
-  kvinhibitor,
-};
+const attachmentsList: Record<string, any> = {};
 
 export function getSniperAttachments(gun: string, count: number): any {
   let attachments: any = {};

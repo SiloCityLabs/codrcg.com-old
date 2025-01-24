da1c103b152191d58d118e82319b83aa4dc3bbb6
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-01-24 17:20:54 +0000

    [ISSUE-60] MW3: vest validation

--- helpers/generator/modern-warfare/three/fetchPerks.tsx
+++ helpers/generator/modern-warfare/three/fetchPerks.tsx
@@ -3,7 +3,7 @@ import bootsList from "@/json/modern-warfare/three/perk/boots.json";
 import gearList from "@/json/modern-warfare/three/perk/gear.json";
 import { randomListItem } from "@/helpers/randomListItem";
 
-export function fetchPerks(): string {
+export function fetchPerks(allowGear2: boolean = true): string {
   const perks: string[] = [];
 
   perks.push(
@@ -12,11 +12,13 @@ export function fetchPerks(): string {
     randomListItem(gearList).name
   );
 
-  let gear2: string;
-  do {
-    gear2 = randomListItem(gearList).name;
-  } while (perks.includes(gear2));
-  perks.push(gear2);
+  if (allowGear2) {
+    let gear2: string;
+    do {
+      gear2 = randomListItem(gearList).name;
+    } while (perks.includes(gear2));
+    perks.push(gear2);
+  }
 
   return perks.join(", ");
 }

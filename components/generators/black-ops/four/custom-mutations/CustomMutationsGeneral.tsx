4de81a14a8caeebf6605384922e5c98a6caef69a
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-01-25 07:10:12 +0000

    [ISSUE-98] BO4: Custom Mutations - General Section updates

--- components/generators/black-ops/four/custom-mutations/CustomMutationsGeneral.tsx
+++ components/generators/black-ops/four/custom-mutations/CustomMutationsGeneral.tsx
@@ -3,36 +3,15 @@ import React, { useEffect, useState } from "react";
 import { Row, Col, Button } from "react-bootstrap";
 import CustomMutationsSimple from "@/components/generators/black-ops/four/custom-mutations/views/CustomMutationsSimple";
 //Types
-import { Setting, CustomMutationsProps } from "@/types/CustomMutations";
+import { CustomMutationsProps } from "@/types/CustomMutations";
 
 function CustomMutationsGeneral({ data, count }: CustomMutationsProps) {
-  const [isLoading, setIsLoading] = useState(false);
-
-  console.log("CustomMutationsGeneral data", data);
-
-  useEffect(() => {
-    setIsLoading(true);
-  }, []);
-
   return (
-    isLoading && (
-      <>
-        <Row className="justify-content-md-center">
-          {data.map((setting, index) => (
-            <React.Fragment key={index}>
-              {setting.type === "simple" ? (
-                <CustomMutationsSimple data={setting} count={count} />
-              ) : (
-                <Col xl={3} lg={4} md={6} sm className="text-center mb-4">
-                  <span className="fw-bolder fs-5">{setting.name}:</span> <br />
-                  <span className="text-muted fs-6">{setting.default}</span>
-                </Col>
-              )}
-            </React.Fragment>
-          ))}
-        </Row>
-      </>
-    )
+    <Row className="justify-content-md-center">
+      {data.map((setting, index) => (
+        <CustomMutationsSimple data={setting} count={count} key={index} />
+      ))}
+    </Row>
   );
 }
 

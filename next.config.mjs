e82a44c49db4acb08e235451c9c5cb79287df77f
Luis Rodriguez <me@luisdrodriguez.com>
2025-01-30 12:30:00 -0500

    testing redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/black-ops-four/generator",
-        destination: "/black-ops/four/generator",
-        permanent: true, // 301 redirect
-      },
-      {
-        source: "/black-ops-four/zombies-generator",
-        destination: "/black-ops/four/zombies/generator",
-        permanent: true, // 301 redirect
-      },
-      {
-        source: "/black-ops-four/info",
-        destination: "/black-ops/four/info",
-        permanent: true, // 301 redirect
-      },
-      {
-        source: "/cold-war/generator",
-        destination: "/black-ops/cold-war/generator",
-        permanent: true, // 301 redirect
-      },
-      {
-        source: "/cold-war/generator",
-        destination: "/black-ops/cold-war/zombies-generator",
-        permanent: true, // 301 redirect
-      },
-      {
-        source: "/cold-war/info",
-        destination: "/black-ops/cold-war/info",
-        permanent: true, // 301 redirect
-      },
-    ];
-  },
 };
 
 export default nextConfig;

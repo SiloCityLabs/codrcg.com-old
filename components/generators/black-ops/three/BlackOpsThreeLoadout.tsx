2036a6ba17c79320713b3d30043bc1e172ed5aa3
Andrew Elbaneh <aelbaneh0615@gmail.com>
2025-02-17 02:21:04 -0500

    IW MP: Cleaned up generator view

--- components/generators/black-ops/three/BlackOpsThreeLoadout.tsx
+++ components/generators/black-ops/three/BlackOpsThreeLoadout.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
-import CodPlaceholder from "@/components/CodPlaceholder";
 import SimpleGeneratorView from "@/components/generators/cod/SimpleGeneratorView";
 import PerkGreedGeneratorView from "@/components/generators/cod/PerkGreedGeneratorView";
 //Helpers

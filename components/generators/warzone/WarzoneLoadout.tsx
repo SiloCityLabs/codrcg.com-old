986c3f7eaf07e4d518dc915f7e53e8f5cbd055d0
aelbaneh0615 <aelbaneh0615@gmail.com>
2025-02-18 16:42:13 +0000

    Warzone: Implemented new generator defaults

--- components/generators/warzone/WarzoneLoadout.tsx
+++ components/generators/warzone/WarzoneLoadout.tsx
@@ -3,6 +3,7 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 import SimpleGeneratorView from "@/components/generators/cod/SimpleGeneratorView";
 //Helpers
 import { implodeObject } from "../../../helpers/implodeObject";
+import { scrollToTop } from "@/helpers/scrollToTop";
 import { fetchWeapon } from "../../../helpers/fetch/fetchWeapon";
 import { fetchPerks } from "../../../helpers/fetch/fetchPerks";
 import { fetchAttachments } from "@/helpers/fetch/fetchAttachments";
@@ -11,51 +12,36 @@ import { fetchWildcard } from "@/helpers/fetch/fetchWildcard";
 import { fetchClassName } from "@/helpers/fetch/fetchClassName";
 //Utils
 import { sendEvent } from "@/utils/gtag";
+//json
+import defaultData from "@/json/cod/default-generator-info.json";
 
 function WarzoneLoadout() {
+  const [isLoading, setIsLoading] = useState(true);
   const [containerClass, setContainerClass] = useState("hidden");
   const [isGenerating, setIsGenerating] = useState(true);
-  const [data, setData] = useState({
-    randClassName: "",
-    perks: null,
-    weapons: {
-      primary: {
-        weapon: { name: "", type: "", game: "", no_attach: false },
-        attachments: "",
-      },
-      secondary: {
-        weapon: { name: "", type: "", game: "", no_attach: false },
-        attachments: "",
-      },
-      melee: { name: "", type: "", game: "" },
-    },
-    equipment: {
-      tactical: { name: "", type: "" },
-      lethal: { name: "", type: "" },
-    },
-    wildcard: { name: "", type: "" },
-  });
+  const [data, setData] = useState(defaultData);
 
   useEffect(() => {
     fetchLoadoutData(setData, setContainerClass);
     setIsGenerating(false);
+    setIsLoading(false);
   }, []);
 
   const handleClick = async () => {
     setIsGenerating(true);
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
 
     setTimeout(() => {
       fetchLoadoutData(setData, setContainerClass);
       setIsGenerating(false);
+      scrollToTop();
     }, 1000);
   };
 
   const { randClassName, perks, weapons, equipment, wildcard } = data;
+
+  if (isLoading) {
+    return <div className="text-center">Loading...</div>;
+  }
 
   return (
     <>

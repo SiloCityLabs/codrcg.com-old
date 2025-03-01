33caa05750311ea6d1d4edda7c18bd0224eecbae
Bana0615 <aelbaneh0615@gmail.com>
2025-03-01 08:49:22 +0000

    [ISSUE-147] BO6: Added new TR2 - Marksman Rifle

--- components/changelog/Changelog2025.tsx
+++ components/changelog/Changelog2025.tsx
@@ -7,6 +7,10 @@ function Changelog2025() {
         <Col>
           <p>
             2/28/2025: <a href="black-ops/six/generator">Black Ops VI:</a> Added
+            new TR2 - Marksman Rifle!
+          </p>
+          <p>
+            2/28/2025: <a href="black-ops/six/generator">Black Ops VI:</a> Added
             new D1.3 Sector weapon!
           </p>
           <p>
